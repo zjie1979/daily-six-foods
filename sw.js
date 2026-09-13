fbdd20437bcbdaaae60884039d40f7dd4a907150
zjie1979 <13360694@qq.com>
2026-09-13 13:06:25 +0800

    Publish daily six food weight tracker

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='daily-six-foods-v2-weight';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./favicon.svg','./icon-192.png','./icon-512.png','./assets/six-foods.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match('./index.html'))));
+});
